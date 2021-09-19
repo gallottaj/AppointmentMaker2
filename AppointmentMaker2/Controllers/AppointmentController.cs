f6795cd13b9463571c739c31f4cee7a992e5542b
Joey <gallo@DESKTOP-BO1GFI1>
2021-09-19 15:42:43 -0700

    added data validation

---
+++ AppointmentMaker2/Controllers/AppointmentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentMaker2.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //method to create new appointment
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- AppointmentMaker2/Models/AppointmentModel.cs
+++ AppointmentMaker2/Models/AppointmentModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,19 @@ namespace AppointmentMaker2.Models
 {
     public class AppointmentModel
     {
+        [Required]
+        [DisplayName("Patient's full name")]
         public string PatientName { get; set; }
+        [DisplayName("Appointment request date")]
         public DateTime AppointmentDate { get; set; }
+        [DisplayName("Patient's net worth")]
+
         public decimal PatientNetWorth { get; set; }
+        [DisplayName("Primary doctor last name")]
+
         public string DoctorName { get; set; }
+        [DisplayName("Patients perceived name (1-10)")]
+
         public int PainLevel { get; set; }
 
     }
