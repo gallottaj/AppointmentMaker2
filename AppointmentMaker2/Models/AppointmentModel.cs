e4b6e5d0a7f9843c8c34c4375afa452f8c736b6e
Joey <gallo@DESKTOP-BO1GFI1>
2021-09-19 15:31:20 -0700

    Add project files.

---
+++ AppointmentMaker2/Models/AppointmentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentMaker2.Models
+{
+    public class AppointmentModel
+    {
+        public string PatientName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public decimal PatientNetWorth { get; set; }
+        public string DoctorName { get; set; }
+        public int PainLevel { get; set; }
+    }
+}
